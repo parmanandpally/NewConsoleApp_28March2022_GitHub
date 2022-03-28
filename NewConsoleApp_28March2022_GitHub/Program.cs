df3f69cd93230f6214e53f84f5bed2526ea68667
nandu <parmanandpally@gmail.com>
2022-03-28 13:50:37 +0530

    Add project files.

---
+++ NewConsoleApp_28March2022_GitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewConsoleApp_28March2022_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
